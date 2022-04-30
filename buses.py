f9a7d7551128427fec5adb98bb4a6276d54e28c3
ADVAGA1 <vaqueradriag@gmail.com>
2022-04-30 12:31:48 +0200

    Update buses.py

--- buses.py
+++ buses.py
@@ -2,9 +2,18 @@ import requests
 import json
 from datetime import datetime, timedelta, date
 
+
+def print_horas(horas_buenas):
+    print("Próximas horas: \n")
+    for time in horas_buenas:
+        tiempo = time.strftime("%H:%M")
+        print(f"{tiempo}\t")
+
+
+
 feiners = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
 
-dia = "Monday" #date.today().strftime("%A")
+dia = date.today().strftime("%A")
 tipus_dia = 0
 if dia in feiners:
     tipus_dia = 1
@@ -36,7 +45,7 @@ print(str(codigo_linea) + "\n")
 
 nom_parada = "Padilla - Mas Casanovas" #input("Que parada? ")
 
-get_parada = requests.get("https://api.tmb.cat/v1/transit/linies/bus/"+str(codigo_linea)+"/parades?app_id=896b5b9e&app_key=8ff3a68902a34aa9e2a6e6ff037f0ff1")
+get_parada = requests.get(f"https://api.tmb.cat/v1/transit/linies/bus/{codigo_linea}/parades?app_id=896b5b9e&app_key=8ff3a68902a34aa9e2a6e6ff037f0ff1")
 
 r = get_parada.json()
 
@@ -57,8 +66,7 @@ hora = input("Que hora? ")
 
 hora_date = datetime.strptime(hora, '%H:%M')
 
-
-get_hora = requests.get("https://api.tmb.cat/v1/transit/linies/bus/"+str(codigo_linea)+"/parades/"+str(codigo_parada)+"/horespas?app_id=896b5b9e&app_key=8ff3a68902a34aa9e2a6e6ff037f0ff1")
+get_hora = requests.get(f"https://api.tmb.cat/v1/transit/linies/bus/{codigo_linea}/parades/{codigo_parada}/horespas?app_id=896b5b9e&app_key=8ff3a68902a34aa9e2a6e6ff037f0ff1")
 
 r = get_hora.json()
 
@@ -67,8 +75,8 @@ horas = []
 contador = 0
 for x in r["features"]:
 
-    if hora_date >= datetime.strptime("00:00",'%H:%M') and hora_date < datetime.strptime("07:00",'%H:%M'):
-        break
+    #if hora_date >= datetime.strptime("00:00",'%H:%M') and hora_date < datetime.strptime("07:00",'%H:%M'):
+     #   break
     #Si eso, mirar de poner el bus mas cercano
 
     if x["properties"]["ID_TIPUS_DIA"] == tipus_dia :
@@ -77,53 +85,36 @@ for x in r["features"]:
         #print(literal)
         if "-" in literal:
             tmp_horas = literal.split(" - ")
-
-            min_hour = datetime.strptime(tmp_horas[0], "%H:%M")
-            max_hour = datetime.strptime(tmp_horas[-1], "%H:%M")
-            if max_hour <= hora_date:
-                continue
-            #print(min_hour, max_hour)
-
-            if hora_date <= min_hour:
-                for times in range(5):
-                    horas.append(tmp_horas[times])
-                #horas = list(map(lambda x: datetime.strptime(x, "%H:%M"), tmp_horas))
-                break
-            elif hora_date > min_hour and hora_date <= max_hour:
-                i = 1
-                while hora_date >= min_hour and i < len(tmp_horas):
-                    min_hour = datetime.strptime(tmp_horas[i], "%H:%M")
-                    i += 1
-                for times in range(5):
-                    if i-1+times < len(tmp_horas):
-                        horas.append(tmp_horas[i-1+times])
-                break
+            for time in tmp_horas:
+                horas.append(datetime.strptime(time,"%H:%M"))
 
         elif "#" in literal:
             info = literal.split(" ")
-
             start = datetime.strptime(info[1], "%H:%M")
             end = datetime.strptime(info[3][:5], "%H:%M")
 
             minutes = int(info[5])
 
-            if hora_date >= start and hora_date <= end:
-                step = start
-                contador = 0
-                while step <= end:
-                    if step <= hora_date:
-                        step += timedelta(minutes=minutes)
-                    else:
-                        horas.append(step)
-                        step += timedelta(minutes=minutes)
-                        contador +=1
-                        if contador > 4:
-                            break
-
-                break
+            step = start
+            while step <= end:
+                horas.append(step)
+                step += timedelta(minutes=minutes)
             #horas = list(filter(lambda x: x >= hora_date, horas))
-    
-if len(horas) == 0:
+
+#print(horas)
+
+horas_buenas = []
+contador = 0
+
+for time in horas:
+    if hora_date < time:    
+        horas_buenas.append(time)
+        contador += 1
+        if contador > 4:
+            break
+
+
+if len(horas_buenas) == 0:
     print("NO HAY BUSES BOBO")
     exit()
-print(horas)
+print_horas(horas_buenas)
