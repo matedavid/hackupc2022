03026f9c90fa4adf120819f5c173a8c3e68e1341
matedavid <david.mate.lopez@gmail.com>
2022-04-30 02:50:53 +0200

    organized code and code for each station

--- main.py
+++ main.py
@@ -1,26 +1,79 @@
 from bs4 import BeautifulSoup
 import requests as req
 import json
+import sys
+from enum import Enum
 
-res = req.request("GET", "https://www.fgc.cat/es/buscador/?from_address=Sarri%C3%A0&from_code=SR&to_address=Pl.+Catalunya&to_code=PC&datetime_option=0&range_option=0&date=29-04-2022&time_from=22%3A29&transport=0")
-if res.status_code != 200:
-  print("Status code:", res.status_code)
-  exit(1)
+class TimeMode(Enum):
+  NOW = 0
+  DEPARTURE = 1
+  ARRIVAL = 2
 
-soup = BeautifulSoup(res.text, 'html.parser')
-trips_js = soup.find(id="form_cercador_JS-js-after")
+def get_url(origin_address: str, destination_address: str, timemode: TimeMode, time: str = "") -> str:
+  origin_code = ""
+  destination_code = ""
 
-text = trips_js.get_text()
-idx = text.find("{")
-json_content_txt = text[idx:-2]
+  stations_code = json.load(open("codigo-estaciones_fgc.json"))
+  for st in stations_code:
+    station_name = st['fields']['nom_estacio']
 
-json_content = json.loads(json_content_txt)
-trips = json_content["result"]["Trips"]
+    if station_name == origin_address:
+      origin_code = st['fields']['inicials']
+    elif station_name == destination_address:
+      destination_code = st['fields']['inicials']
 
-for t in trips:
-  trip = t['steps'][0]
-  route = trip["route"]
-  departure = trip["departure"]
-  arrival = trip["arrival"]
+  date = "29-04-2022"
 
-  print(f"{route}: {departure}-{arrival}")
+  url = f"https://www.fgc.cat/es/buscador/?from_address={origin_address}&from_code={origin_code}&to_address={destination_address}&to_code={destination_code}&datetime_option={timemode.value}&date={date}"
+  if timemode != TimeMode.NOW:
+    url += f"&time_from={time}"
+
+  return url
+
+def get_trips(url):
+  res = req.get(url)
+  if res.status_code != 200:
+    print("Status code:", res.status_code)
+    exit(1)
+
+  soup = BeautifulSoup(res.text, 'html.parser')
+  trips_js = soup.find(id="form_cercador_JS-js-after")
+
+  text = trips_js.get_text()
+  idx = text.find("{")
+  json_content_txt = text[idx:-2]
+
+  json_content = json.loads(json_content_txt)
+  trips = json_content["result"]["Trips"]
+
+  for t in trips:
+    trip = t['steps'][0]
+    route = trip["route"]
+    departure = trip["departure"]
+    arrival = trip["arrival"]
+
+    print(f"{route}: {departure}-{arrival}")
+
+if __name__ == "__main__":
+  args = sys.argv
+
+  origin = args[1]
+  destination = args[2]
+  timemode = args[3]
+
+  print(origin, destination, timemode)
+
+  if timemode == "now":
+    timemode = TimeMode.NOW
+  elif timemode == "departure":
+    timemode = TimeMode.DEPARTURE
+  elif timemode == "arrival":
+    timemode = TimeMode.ARRIVAL
+
+  time = ""
+  if timemode != TimeMode.NOW:
+    time = args[4]
+
+  url = get_url(origin, destination, timemode, time)
+  print(url)
+  get_trips(url)
