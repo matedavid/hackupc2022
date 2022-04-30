73a0ec7aaf11ed5b536d5da5bae0a2b902856aa5
matedavid <david.mate.lopez@gmail.com>
2022-04-30 12:42:23 +0200

    adding fgc entries to user

--- init_database.py
+++ init_database.py
@@ -7,7 +7,19 @@ if "database.db" in os.listdir("."):
 con = sqlite3.connect("database.db")
 cur = con.cursor()
 
-cur.execute("CREATE TABLE User (id serial primary key, email varchar(40) not null, password varchar(40) not null);")
+cur.execute("CREATE TABLE User (id integer primary key autoincrement, email varchar(40) not null, password varchar(40) not null);")
+
+cur.execute("""CREATE TABLE FGCTransport (
+  fgcId integer primary key autoincrement, 
+  user int references User,
+
+  originStation varchar(20) not null,
+  originStationCode char(2) not null,
+
+  destinationStation varchar(20) not null,
+  destinationStationCode char(2) not null,
+
+  time time);""")
 con.commit()
 
 con.close()
--- webserver.py
+++ webserver.py
@@ -1,6 +1,7 @@
 from flask import Flask, request 
 import sqlite3
 import json
+from fgc import get_station_code
 
 app = Flask(__name__)
 
@@ -41,13 +42,52 @@ def get_users():
 
   print(users)
   return "Ok"
+
+@app.route("/api/fgc/", methods=["GET"])
+def get_fgc():
+  con = sqlite3.connect("database.db")
+  cur = con.cursor()
+
+  cur.execute("SELECT * FROM FGCTransport;")
+  fgcs = cur.fetchall()
+
+  con.close()
+  print(fgcs)
+
+  return "Ok"
 ### 
 
 @app.route("/api/fgc/add", methods=["POST"])
 def add_fgc_entry():
   data = json.loads(request.data)
-  print(data)
-  return "Returned"
+
+  origin = data["origin"]
+  destination = data["destination"]
+  time = data["time"]
+  user = data["user"]
+
+  origin_code = get_station_code(origin)
+  destination_code = get_station_code(destination)
+
+  con = sqlite3.connect("database.db")
+  cur = con.cursor()
+
+  # TODO: Any check before inserting?
+  # - Check user exists?
+
+  cur.execute("""INSERT INTO FGCTransport (
+      user, 
+      originStation, 
+      originStationCode, 
+      destinationStation, 
+      destinationStationCode,
+      time
+    ) VALUES (?, ?, ?, ?, ?, ?)""", (user, origin, origin_code, destination, destination_code, time))
+
+  con.commit()
+  con.close()
+
+  return response(True)
 
 if __name__ == "__main__":
   app.run(debug=True)
